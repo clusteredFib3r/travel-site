87a19f80fd6f496562d3e9fdc3fe54abed7bb2e3
clusteredFib3r <clusteredFiber@gmail.com>
2018-09-18 13:25:16 +0100

    Started Applying CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
   console.log('Hooray - you created a gulp task!');
@@ -10,17 +16,31 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-  gulp.src('./app/assets/styles/styles.css');
+  return gulp.src('./app/assets/styles/style.css')
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function(){
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
 });
